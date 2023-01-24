bcafb7b6c33e65f0e0703dbb15a1cb0de5a23c2f
Keian Anthony <KeianAnthony1@gmail.com>
2023-01-24 11:17:51 -0800

    home edit

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -7,11 +7,11 @@ function Home() {
                 <h2 className='sec-title'>What is gift ideas?</h2>
                 <p>Gift ideas is a website created for those families or groups of friends who aren't sure what to get someone else.</p>
                 {/* Maybe add some css styling to indicate a different section */}
-                <h2 className='sec-title'>Gift Confusion?</h2>
+                <h2 className='sec-title'>Ever Have Gift Questions?</h2>
                 <p>If you have ever been lost when Christmas or a Birthday rolls around as to what someone would enjoy, then you are in the right spot.</p>
                 {/* Maybe add some css styling to indicate a different section */}
-                <h2 className='sec-title'>Section 3</h2>
-                <p>Description for more elaboration.</p>
+                <h2 className='sec-title'>What We Do</h2>
+                <p>Creating a free account allows users to create groups for any occasion! Invite your friends or family to these groups and add things you want to your own list, without ever being able to directly see what anyone else is getting you from your list to ensure the "suprise" factor, and anonymity (if desired), remains!</p>
             </div>
         </div>
     );
