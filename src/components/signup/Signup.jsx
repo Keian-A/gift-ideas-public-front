60ee50ecf3c175006e0208d2492d945e35c85ed7
Keian Anthony <KeianAnthony1@gmail.com>
2023-01-25 10:48:08 -0800

    signup working, need to use resulting data to change global state in redux now

--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
+import axios from 'axios';
 import './Signup.css';
+
+const SERVER = process.env.REACT_APP_SERVER;
 
 function Signup() {
     const [username, setUsername] = useState("");
@@ -12,11 +15,30 @@ function Signup() {
     const [phoneNumber, setPhoneNumber] = useState("");
     const [birthday, setBirthday] = useState("");
     const [show, setShow] = useState(false);
+    const [showError, setShowError] = useState(false);
 
-    const formSubmit = (e) => {
+    const formSubmit = async (e) => {
         e.preventDefault();
         // Send req to backend here
-        console.log(username, password, cPassword, email, firstname, lastname, phoneNumber, birthday);
+        try {
+            if (password === cPassword) {
+                let newUser = {
+                    username,
+                    password,
+                    email,
+                    firstname,
+                    lastname,
+                    phoneNumber,
+                    birthday
+                }
+                let result = await axios.post(`${SERVER}/newUser`, newUser);
+                console.log(result);
+            } else {
+                setShowError(true);
+            }
+        } catch (e) {
+            console.error(e.message);
+        }
     }
 
     const handleChange = (e) => {
@@ -63,12 +85,17 @@ function Signup() {
                     <div className='inputSection'>
                         <input placeholder='*Username' className='formInput' name="username" required onChange={handleChange}></input>
                     </div>
+                    {showError ? (
+                        <div className='inputSection'>
+                            <h4 className='passwordError'>Password fields don't match</h4>
+                        </div>
+                    ) : null}
                     <div className='inputSection'>
                         <input placeholder='*Password' className='formInput' type={show ? "text" : "password"} name="password" required onChange={handleChange}></input>
                     </div>
                     <div className='inputSection'>
                         <input placeholder='*Confirm Password' className='formInput' type={show ? "text" : "password"} name="confirm-password" required onChange={handleChange}></input>
-                        <button onClick={changePassShow}>Show Password</button>
+                        <button onSubmit="return false;" onClick={changePassShow}>Show Password</button>
                     </div>
                     <div className='inputSection'>
                         <input placeholder='*Email' className='formInput' name="email" type="text" required onChange={handleChange}></input>
