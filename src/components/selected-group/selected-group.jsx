7561aeb1674c0c92b23ee29319adc3c582b71d61
Keian Anthony <KeianAnthony1@gmail.com>
2023-11-07 13:20:38 -0800

    display for groupdata

--- src/components/selected-group/selected-group.jsx
+++ src/components/selected-group/selected-group.jsx
@@ -3,6 +3,7 @@ import './selected-group.css';
 function SelectedGroup({ groupID, groupData }) {
     return (
         <div className='selected-group'>
+            {console.log(groupData)}
             <h2>Selected Group: {groupID}</h2>
         </div>
     )
